bb7c0b2bed5cb7fc0de4517fd29979191ff0ec79
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-01-31 12:47:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -73,12 +73,8 @@ Soon
   }
 });
 
-client.on("message", message => {
-  if (message.content === "السلام عليكم") {
-message.channel.send(`**
-وعليكم السلام ورحمة الله وبركاته
-**`)
-      message.channel.send("اهلا فيك منوور")
-  }
-});
 
+client.on('guildMemberAdd', member => {
+    const guild = member.guild;
+    guild.channels.find(channel => channel.name === "未来welcome未来").send("اهلا وسهلا بك في سيرفرنا منور "+member.user.username);
+});
